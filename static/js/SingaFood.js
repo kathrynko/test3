9d65fd6032417ce0b262d32c5483987061b370f8
huiqin <gaohuiqin@foxmail.com>
2015-09-30 16:30:00 +0200

    a lot of changes

---
+++ static/js/SingaFood.js
@@ -0,0 +1,157 @@
+
+/*
+// ############
+//    STEP 1
+// ############
+*/
+
+// If the element with id 'link1' is clicked
+$('#link1').on('click', function(e){
+	var bootstrapCSSLink = $('<link rel="stylesheet" type="text/css" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">');
+	var bootstrapThemeCSSLink = $('<link rel="stylesheet" type="text/css" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">');
+	var bootstrapJavaScriptLink = $('<script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>');
+
+	$('body').append(bootstrapCSSLink);
+	$('body').append(bootstrapThemeCSSLink);
+	$('body').append(bootstrapJavaScriptLink);
+});
+
+
+
+/*
+// ############
+//    STEP 13
+// ############
+*/
+
+$('#link13').on('click', function(e){
+
+	var query = $('#query13').text();
+	var endpoint = 'http://localhost:5820/SingaFood/query';
+	var format = 'JSON';
+
+	$.get('/sparql',data={'endpoint': endpoint, 'query': query, 'format': format}, function(json){
+		var pre = $('<pre></pre>');
+		pre.text(JSON.stringify(json));
+		$('#linktarget13').html(pre);
+	});
+
+});
+
+
+/*
+// ############
+//    STEP 14
+// ############
+*/
+
+$('#link14').on('click', function(e){
+
+	var query = $('#query14').val();
+	var endpoint = 'http://localhost:5820/SingaFood/query';
+	var format = 'JSON';
+
+	$.get('/sparql',data={'endpoint': endpoint, 'query': query, 'format': format}, function(json){
+		console.log(json);
+
+		try {
+			var vars = json.head.vars;
+
+			var ul = $('<ul></ul>');
+			ul.addClass('list-group');
+
+			$.each(json.results.bindings, function(index,value){
+				var li = $('<li></li>');
+				li.addClass('list-group-item');
+
+				$.each(vars, function(index, v){
+					var v_type = value[v]['type'];
+					var v_value = value[v]['value'];
+
+					li.append('<strong>'+v+'</strong><br/>');
+
+					// If the value is a URI, create a hyperlink
+					if (v_type == 'uri') {
+						var a = $('<a></a>');
+						a.attr('href',v_value);
+						a.text(v_value);
+						li.append(a);
+					// Else we're just showing the value.
+					} else {
+						li.append(v_value);
+					}
+					li.append('<br/>');
+
+				});
+				ul.append(li);
+
+			});
+
+			$('#linktarget14').html(ul);
+		} catch(err) {
+			$('#linktarget14').html('Something went wrong!');
+		}
+
+
+
+	});
+
+});
+/*
+// ############
+//    STEP 15
+// ############
+*/
+
+$('#link15').on('click', function(e){
+
+	var query = $('#query15').val();
+	var endpoint = 'http://localhost:5820/SingaFood/query';
+	var format = 'JSON';
+
+	$.get('/sparql2',data={'endpoint': endpoint, 'query': query, 'format': format}, function(json){
+		console.log(json);
+
+		try {
+			var vars = json.head.vars;
+
+			var ul = $('<ul></ul>');
+			ul.addClass('list-group');
+
+			$.each(json.results.bindings, function(index,value){
+				var li = $('<li></li>');
+				li.addClass('list-group-item');
+
+				$.each(vars, function(index, v){
+					var v_type = value[v]['type'];
+					var v_value = value[v]['value'];
+
+					li.append('<strong>'+v+'</strong><br/>');
+
+					// If the value is a URI, create a hyperlink
+					if (v_type == 'uri') {
+						var a = $('<a></a>');
+						a.attr('href',v_value);
+						a.text(v_value);
+						li.append(a);
+					// Else we're just showing the value.
+					} else {
+						li.append(v_value);
+					}
+					li.append('<br/>');
+
+				});
+				ul.append(li);
+
+			});
+
+			$('#linktarget15').html(ul);
+		} catch(err) {
+			$('#linktarget15').html('Something went wrong!');
+		}
+
+
+
+	});
+
+});
